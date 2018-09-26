6147e63de1769226eec622701f85e228efb7cd50
Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>
2018-09-26 18:03:38 -0300

    first commit

---
+++ engine/src/application.rs
@@ -0,0 +1,12 @@
+
+pub struct Application {
+    pub t: i32
+}
+
+impl Application {
+    pub fn testApp(self, phrase : &str) {
+        println!("{}", phrase);
+    }
+
+    
+}
---
+++ src/main.rs
@@ -0,0 +1,10 @@
+extern crate engine;
+
+use engine::application;
+
+fn main() {
+
+    let app = application::Application { t: 32 };
+
+    app.testApp("aosidoaijsdjasd");
+}
